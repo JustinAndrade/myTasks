598cbef01e853791aa1be359eba5d84a00641955
justinandrade <j.andrade.john@gmail.com>
2020-04-29 22:36:02 -0400

    basic redux implemented

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import "./App.css";
 
-function App() {
+function App(props) {
+  const [goals, setGoals] = useState([]);
+  const [goal, setGoal] = useState({
+    name: "",
+    type: "",
+    requirement: "",
+    plan: "",
+    completed: false,
+  });
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setGoals([...goals, goal]);
+  };
+
+  const changeHandler = (e) => {
+    e.preventDefault();
+    setGoal({ ...goal, [e.target.name]: e.target.value });
+  };
+
+  console.log(props);
+  console.log(goals);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Caroline's Goals App</h1>
       </header>
+      <form onSubmit={submitHandler}>
+        <input
+          name="name"
+          value={goal.name}
+          onChange={changeHandler}
+          placeholder="name"
+        />
+        <input name="type" onChange={changeHandler} placeholder="name" />
+        <input
+          name="requirement"
+          onChange={changeHandler}
+          placeholder="requirements"
+        />
+        <input
+          name="plan"
+          onChange={changeHandler}
+          placeholder="plan for success"
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <div>
+        {goals.map((goal) => {
+          return (
+            <div>
+              <h2>{goal.name}</h2>
+              <span>{goal.type}</span>
+              <p>{goal.requirement}</p>
+              <p>{goal.plan}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    goals: state.goals,
+  };
+};
+
+export default connect(mapStateToProps)(App);
---
+++ todo/src/redux/reducers.js
@@ -0,0 +1,16 @@
+const initialState = {
+  goals: [
+    {
+      name: "Finish School",
+    },
+  ],
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+module.exports = reducer;
